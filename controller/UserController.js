852556de02883046148781ab501a43cd26700fcb
Paulo Renan Almeida <able.paulorenan@gmail.com>
2022-02-07 17:26:16 -0300

    req1

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,10 +1,22 @@
+const jwt = require('jsonwebtoken');
+
 const UserService = require('../services/UserService');
+
+const secret = 'secret';
 
 const adicionarUsuario = async (req, res) => {
   const user = req.body;
   console.log(user);
   const response = await UserService.adicionarUsuario(user);
-  res.status(response.status).json(response);
+  if (response.status !== 200) {
+    return res.status(response.status).json({ message: response.message });
+  }
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+  const token = jwt.sign({ data: user }, secret, jwtConfig);
+  return res.status(201).json({ token });
 };
 
 module.exports = {
